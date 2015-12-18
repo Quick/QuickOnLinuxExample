889f2058dbb53ac8ea31122ff2a441d47b483e95
Brian Croom <bcroom@pivotal.io>
2015-12-18 16:45:34 +0000

    Update to use Nimble matchers

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "QuickTest",
     dependencies: [
-        .Package(url: "https://github.com/briancroom/Quick.git", majorVersion: 0, minor: 8)
+        .Package(url: "https://github.com/briancroom/Quick.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/briancroom/Nimble.git", majorVersion: 3, minor: 1),
     ]
 )
--- Sources/MySpec.swift
+++ Sources/MySpec.swift
@@ -1,16 +1,26 @@
 import Quick
-import XCTest
+import Nimble
 
 class MySpec: QuickSpec {
   override func spec() {
     describe("the test suite") {
       it("runs my `it` block") {
-        XCTAssert(true)
+        expect(true).to(beTruthy())
       }
 
       context("when running another example") {
         it("should register errors") {
-          XCTAssertEqual(1, 2, "Uh oh fail!")
+          expect(1).to(equal(2))
+        }
+      }
+
+      describe("additional matchers") {
+        it("should allow comparison") {
+          expect(42).to(beGreaterThan(1))
+        }
+
+        it("should allow checking count") {
+          expect([1, 2, 3]).to(haveCount(3))
         }
       }
     }
